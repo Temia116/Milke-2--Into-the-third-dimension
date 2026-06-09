370217ee05dfffa42fdd28811081d0ebf5fd9681
Connor <contod21@kingshigh.school.nz>
2026-06-10 11:34:16 +1200

    Fart Ball Chud

--- van.gd
+++ van.gd
@@ -11,10 +11,12 @@ func _ready():
 
 func _process(delta):
 	position.x += speed * direction * delta
-	if position.x >= right_limit:
+	if position.x >= right_limit and direction == 1.0:
 		direction = -1.0
-	elif position.x <= left_limit:
+		$van_1.rotation.y = PI
+	elif position.x <= left_limit and direction == -1.0:
 		direction = 1.0
+		$van_1.rotation.y = 0.0
 
 func _on_catch_zone_body_entered(body):
 	if body is RigidBody3D and not body.exited:
